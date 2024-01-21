9ce0578bfd3c6db023a2590e15bf2e6a5d126f46
eminencegrs <a.dariienko@outlook.com>
2024-01-21 21:11:27 +0100

    Improve Chapter 01 08.

--- src/FPinFSharp.App/Chapter_01/Chapter_01_08.fs
+++ src/FPinFSharp.App/Chapter_01/Chapter_01_08.fs
@@ -3,7 +3,9 @@ namespace FPinFSharp.App.Chapter_01
 // 1.8 Euclid’s algorithm (for computing the greatest common divisor of two natural numbers).
 module Chapter_01_08 =
 
+    // Euclid's algorithm is implemented by using pattern matching to handle the base case (when `m` is 0)
+    // and recursively updating the values of `m` and `n` until the base case is reached.
     let rec gcd (m, n) =
         match (m, n) with
-        | (0, n) -> n
-        | (m, n) -> gcd(n % m, m)
+        | 0, n -> n
+        | m, n -> gcd(n % m, m)
--- tests/FPinFSharp.App.UnitTests/Chapter_01/Chapter_01_08_Tests.fs
+++ tests/FPinFSharp.App.UnitTests/Chapter_01/Chapter_01_08_Tests.fs
@@ -6,8 +6,11 @@ open Xunit
 module Chapter_01_08_Tests =
 
     [<Theory>]
+    [<InlineData(6, 8, 2)>]
     [<InlineData(12, 27, 3)>]
     [<InlineData(36, 116, 4)>]
+    [<InlineData(100, 20, 20)>]
+    [<InlineData(20, 100, 20)>]
     let ``Should get the greatest common divisor of two natural numbers`` (m, n, expectedResult) =
         let actualResult = gcd(m, n)
         Assert.Equal(expectedResult, actualResult)
