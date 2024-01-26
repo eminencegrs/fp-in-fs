16f44d792e062c9a51b49085fc44f38ff9c4ca9a
eminencegrs <a.dariienko@outlook.com>
2024-01-26 22:01:51 +0100

    Add Section 02 04.

---
+++ tests/FPinFSharp.Exercises.UnitTests/Chapter_02/Chapter_02_04_Tests.fs
@@ -0,0 +1,17 @@
+namespace FPinFSharp.Exercises.UnitTests.Chapter_02
+
+open FPinFSharp.Exercises.Chapter_02.Chapter_02_04
+open Shouldly
+open Xunit
+
+module Chapter_02_04_Tests =
+
+    [<Theory>]
+    [<InlineData(6, 8, 2)>]
+    [<InlineData(12, 27, 3)>]
+    [<InlineData(36, 116, 4)>]
+    [<InlineData(100, 20, 20)>]
+    [<InlineData(20, 100, 20)>]
+    let ``Should get the greatest common divisor of two natural numbers`` (m, n, expectedResult) =
+        let actualResult = gcd(m, n)
+        actualResult.ShouldBe(expectedResult)
